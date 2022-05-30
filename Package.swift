f91437e26bbbc0dcdfdd8d565c92aa162439629c
Daniel Saidi <daniel.saidi@gmail.com>
2022-05-30 13:25:48 +0200

    Remove Quick and Nimble from package dependencies

--- Package.swift
+++ Package.swift
@@ -18,8 +18,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "10.0.0")),
         .package(url: "https://github.com/danielsaidi/MockingKit.git", .upToNextMajor(from: "1.1.0"))
     ],
     targets: [
@@ -28,6 +26,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "RichTextKitTests",
-            dependencies: ["RichTextKit", "Quick", "Nimble", "MockingKit"]),
+            dependencies: ["RichTextKit", "MockingKit"]),
     ]
 )
