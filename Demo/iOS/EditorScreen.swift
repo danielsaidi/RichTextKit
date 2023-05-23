e915a2de46b4a8c40089822e7f4a0469d52dd9d7
Irfaan <57593842+IRFAANFTK@users.noreply.github.com>
2023-05-23 09:01:41 +0400

    Update EditorScreen.swift
    Added function to clear the text editor

--- Demo/iOS/EditorScreen.swift
+++ Demo/iOS/EditorScreen.swift
@@ -20,6 +20,15 @@ struct EditorScreen: View {
 
     @StateObject
     var context = RichTextContext()
+    
+    @State
+    private var showConfirmationAlert = false
+    
+    @FocusState
+    private var isEditorFocused: Bool
+    
+    @State
+    private var refreshCount = 0
 
     var body: some View {
         VStack {
@@ -34,18 +43,41 @@ struct EditorScreen: View {
             }
         }
         .viewDebug()
+        .id(refreshCount) // Use id to force view refresh
     }
 }
 
 private extension EditorScreen {
 
     var editor: some View {
-        RichTextEditor(text: $text, context: context) {
-            $0.textContentInset = CGSize(width: 10, height: 20)
+        ZStack(alignment: .bottomTrailing) {
+            RichTextEditor(text: $text, context: context) {
+                $0.textContentInset = CGSize(width: 10, height: 20)
+            }
+            .background(Material.regular)
+            .cornerRadius(5)
+            .focused($isEditorFocused)
+            .focusedValue(\.richTextContext, context)
+
+            Button(action: {
+                showConfirmationAlert = true
+            }) {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(.red)
+                    .padding(10)
+            }
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 16, trailing: 16))
         }
-        .background(Material.regular)
-        .cornerRadius(5)
-        .focusedValue(\.richTextContext, context)
+        .alert(isPresented: $showConfirmationAlert) {
+            Alert(
+                title: Text("Clear"),
+                message: Text("Are you sure you want to clear the text?"),
+                primaryButton: .default(Text("Clear")) {
+                    clearContent()
+                },
+                secondaryButton: .cancel()
+            )
+        }
     }
 
     var toolbar: some View {
@@ -55,6 +87,16 @@ private extension EditorScreen {
             trailingButtons: {}
         )
     }
+    
+    // Function to clear the content of the editor by setting text to empty string and refreshing the view
+    private func clearContent() {
+      isEditorFocused = false // Editor should not be focused to allow view refresh
+      DispatchQueue.main.async {
+        text = NSAttributedString(string: "")
+        refreshCount += 1  // Increment refreshCount to trigger view refresh
+      }
+    }
+
 }
 
 struct EditorScreen_Previews: PreviewProvider {
