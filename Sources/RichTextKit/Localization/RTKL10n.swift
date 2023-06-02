0085a804d453b8c10fa63550d16ca7bd8aef08e7
Daniel Saidi <daniel.saidi@gmail.com>
2023-06-02 11:42:32 +0200

    Add localizable strings

--- Sources/RichTextKit/Localization/RTKL10n.swift
+++ Sources/RichTextKit/Localization/RTKL10n.swift
@@ -43,6 +43,8 @@ public enum RTKL10n: String, CaseIterable, Identifiable {
     menuFontLarger,
     menuFontSmaller,
     menuFormat,
+    menuIndentIncrease,
+    menuIndentDecrease,
     menuPrint,
     menuSave,
     menuSaveAs,
