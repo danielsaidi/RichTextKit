84d93e6ee99dd84d5c25221c2d11ec7ef6f64c24
Dominik Bucher <dbucher@paylocity.com>
2024-02-13 13:11:15 +0100

    Run swiftformat

--- Sources/RichTextKit/RichTextView+Config_AppKit.swift
+++ Sources/RichTextKit/RichTextView+Config_AppKit.swift
@@ -1,6 +1,6 @@
 //
 //  RichTextView+Config_AppKit.swift
-//  
+//
 //
 //  Created by Dominik Bucher on 13.02.2024.
 //
@@ -9,12 +9,12 @@
 import Foundation
 
 public extension RichTextView {
-    
+
     /**
      This type can be used to configure a ``RichTextEditor``.
      */
     struct Configuration {
-        
+
         /// Create a custom configuration
         /// - Parameters:
         ///   - isScrollingEnabled: Whether or not the editor should scroll, by default `true`.
@@ -26,7 +26,7 @@ public extension RichTextView {
             self.isScrollingEnabled = isScrollingEnabled
             self.isContinuousSpellCheckingEnabled = isContinuousSpellCheckingEnabled
         }
-        
+
         /// Whether or not the editor should scroll.
         public var isScrollingEnabled: Bool
         /// Whether the editor spell-checks in realtime.
