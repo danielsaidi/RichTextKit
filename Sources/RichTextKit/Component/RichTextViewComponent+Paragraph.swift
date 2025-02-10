aaf66b13f51a19fcbbcd571056b8e019bfd4a9c2
Rizwana <rizwana@simformsolutions.com>
2025-02-10 22:44:58 +0530

    Fixed Issue where alignment wasn't applying for entire selected range Added undo for alignment and header

--- Sources/RichTextKit/Component/RichTextViewComponent+Paragraph.swift
+++ Sources/RichTextKit/Component/RichTextViewComponent+Paragraph.swift
@@ -29,7 +29,7 @@ public extension RichTextViewComponent {
     /// selected paragraphs. If many paragraphs are selected,
     /// it will only affect the first one.
     func setRichTextParagraphStyle(_ style: NSParagraphStyle) {
-        let range = lineRange(for: selectedRange)
+        let range = selectedRange
         guard range.length > 0 else {
             typingAttributes[.paragraphStyle] = style
             return
@@ -37,7 +37,36 @@ public extension RichTextViewComponent {
         #if os(watchOS)
         setRichTextAttribute(.paragraphStyle, to: style, at: range)
         #else
+        registerUndo(for: range, style: style)
         textStorageWrapper?.addAttribute(.paragraphStyle, value: style, range: range)
         #endif
+    }
+
+    func registerUndo(for range: NSRange, style: NSParagraphStyle) {
+        guard range.length > 0 else { return }
+        let textView = self as? RichTextView
+
+        #if canImport(UIKit)
+        let undoManager = textView?.undoManager
+        #elseif canImport(AppKit)
+        let undoManager = textView?.undoManager
+        #endif
+
+        guard let undoManager = undoManager, let text = mutableRichText, let textView else {
+            print("Undo Manager/mutableRichText/textview is nil")
+            return
+        }
+
+        // Store the previous paragraph style
+        let currentAttributes = NSAttributedString(attributedString: text.attributedSubstring(from: selectedRange))
+
+        undoManager.registerUndo(withTarget: textView) { target in
+            print("Performing undo, restoring previous style")
+            target.mutableRichText?.replaceCharacters(in: range, with: currentAttributes)
+        }
+
+        undoManager.setActionName("Change Paragraph Style")
+
+
     }
 }
